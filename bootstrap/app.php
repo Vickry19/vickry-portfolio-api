88ccff5bad3469b0afa0e960bf222353b7d45569
vickry19 <vickrykamaluddin2119@gmail.com>
2026-09-10 10:50:12 +0700

    fix: redirect guest ke admin.login, bukan default login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
+        $middleware->redirectGuestsTo(fn () => route('admin.login'));
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
